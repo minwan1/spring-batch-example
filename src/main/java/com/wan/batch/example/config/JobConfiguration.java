67f0b773c68f413fd98385e7e57225b612664be3
Wan <ansatgol@gmail.com>
2019-05-01 17:06:09 +0900

    간단한Job만들기-Job-parameter (#2)
    * Add gradle setting
    
    * Add mysql setting, docker compose
    
    * Add mysql setting 2
    
    * Add simple job
    
    * Add two step
    
    * Add document

--- src/main/java/com/wan/batch/example/config/JobConfiguration.java
+++ src/main/java/com/wan/batch/example/config/JobConfiguration.java
@@ -6,8 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,17 +25,33 @@ public class JobConfiguration {
     @Bean
     public Job job(){
         return jobBuilderFactory.get("job")
-                .start(step1())
+                .start(step1(null))
+                .next(step2(null))
                 .build();
     }
 
     @Bean
-    public Step step1(){
+    @JobScope
+    public Step step1(@Value("#{jobParameters[date]}") String date){
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    log.info(">>> This is Step1");
+                    log.info(">>>>> This is step1");
+                    log.info(">>>>> date = {}", date);
                     return RepeatStatus.FINISHED;
                 }).build();
+    }
+
+
+    @Bean
+    @JobScope
+    public Step step2(@Value("#{jobParameters[date]}") String date) {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is step2");
+                    log.info(">>>>> date = {}", date);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
     }
 
 }
